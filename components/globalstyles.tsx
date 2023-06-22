f7367f67f0b0c39cfef7270be268689d8ecb2f19
sarangchouguley <sarangchouguley284@gmail.com>
2023-06-23 00:57:42 +0530

    initial setup

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,23 +1,50 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    color: ${({ theme }) => theme.colors.primary};
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
+html {
+  scroll-behavior: smooth;
+}
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+* {
+  box-sizing: border-box;
+}
 
-  * {
-    box-sizing: border-box;
+body{
+  background-color: #040714;
+  color: #f9f9f9;
+
+  font-family: Avenir-Roman, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  color: #f9f9f9;
+  text-decoration: none;
+}
+
+/* media queries */
+
+/* Laptop screen */
+@media only screen and (min-width: 768px) {
+  body{
+      font-size: 16px;
   }
+}
+
+/* Tab screen */
+@media only screen and (min-width: 480px) and (max-width: 768px) {
+  body{
+      font-size: 15px;
+  }
+}
+
+/* Mobile screen */
+@media only screen and (max-width: 749px) {
+  body{
+      font-size: 14px;
+  }
+}
 `
 
 export default GlobalStyle
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,14 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
 import GlobalStyle from '../components/globalstyles'
+import Header from '../components/header'
 
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <GlobalStyle/>
+      <Header/>
+      <Component {...pageProps} />
     </>
   )
 }
